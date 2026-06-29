fe268000cfc8ed402bca5407d33f3fe01079d9d3
Ludmila (DeFinn) <ludmila.omlopes@gmail.com>
2026-06-29 07:45:49 -0300

    add filazo and discord links

--- lib/site.ts
+++ lib/site.ts
@@ -7,5 +7,6 @@ export const SITE = {
   GH_URL: "https://github.com/ludmila-omlopes",
   X_URL: "https://x.com/ludylops",
   TH_URL: "https://www.threads.com/@ludylops",
+  DC_URL: "https://discord.gg/qGHZ",
   LUDYLOPS_LIVE: "https://ludylops.live",
 } as const;
